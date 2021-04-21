1041672744188f66b0d5a49efad5d65d545616a0
ohmji <jirawatk1999@hotmail.com>
2021-04-21 16:44:54 +0700

    upda

--- heroku/main/preload.py
+++ heroku/main/preload.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+# import tensorflow as tf
 from PIL import Image
 from io import BytesIO
 # from deepface import DeepFace
@@ -7,7 +7,7 @@ with open("image.jpeg", "rb") as image:
     f = image.read()
     b = bytearray(f)
     img = Image.open(BytesIO(b))
-image_array = tf.keras.preprocessing.image.img_to_array(img)
+# image_array = tf.keras.preprocessing.image.img_to_array(img)
 
 # demography = DeepFace.analyze(image_array, actions=["emotion"])
 # print("Age: ", demography["age"])
