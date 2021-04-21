5f56b3d8ffa8ab53942ebcfad98d0829d8519960
ohmji <jirawatk1999@hotmail.com>
2021-04-21 15:57:42 +0700

    test

--- heroku/main/preload.py
+++ heroku/main/preload.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from PIL import Image
 from io import BytesIO
-from deepface import DeepFace
+# from deepface import DeepFace
 
 with open("image.jpeg", "rb") as image:
     f = image.read()
@@ -9,12 +9,12 @@ with open("image.jpeg", "rb") as image:
     img = Image.open(BytesIO(b))
 image_array = tf.keras.preprocessing.image.img_to_array(img)
 
-demography = DeepFace.analyze(image_array, actions=["emotion"])
+# demography = DeepFace.analyze(image_array, actions=["emotion"])
 # print("Age: ", demography["age"])
 # print("Gender: ", demography["gender"])
 # print("Emotion: ", demography["dominant_emotion"])
 # print("Race: ", demography["dominant_race"])
 
 
-result = DeepFace.verify(image_array, image_array, model_name="Facenet")
-print("Is verified: ", result["verified"])
+# result = DeepFace.verify(image_array, image_array, model_name="Facenet")
+# print("Is verified: ", result["verified"])
