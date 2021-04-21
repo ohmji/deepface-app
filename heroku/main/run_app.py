947080da6b3a44435829202773d221ab75cb61ab
ohmji <jirawatk1999@hotmail.com>
2021-04-21 15:45:36 +0700

    update

--- heroku/main/run_app.py
+++ heroku/main/run_app.py
@@ -2,7 +2,7 @@ import io
 from PIL import Image
 from typing import List
 from fastapi import FastAPI, File, UploadFile
-from deepface import DeepFace
+# from deepface import DeepFace
 from tensorflow.python.framework import ops
 import tensorflow as tf
 
@@ -28,28 +28,28 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/analyze/")
-async def analyzer(file: bytes = File(...)):
+# @app.post("/analyze/")
+# async def analyzer(file: bytes = File(...)):
 
-    image = Image.open(io.BytesIO(file))
-    img = tf.keras.preprocessing.image.img_to_array(image)
+#     image = Image.open(io.BytesIO(file))
+#     img = tf.keras.preprocessing.image.img_to_array(image)
 
-    with graph.as_default():
+#     with graph.as_default():
 
-        demography = DeepFace.analyze(img, actions=["emotion"])
-    return {"file_size": len(file), "prediction": demography}
+#         demography = DeepFace.analyze(img, actions=["emotion"])
+#     return {"file_size": len(file), "prediction": demography}
 
 
-@app.post("/verification/")
-async def verification_route(file: List[bytes] = File(...)):
+# @app.post("/verification/")
+# async def verification_route(file: List[bytes] = File(...)):
 
-    image1 = Image.open(io.BytesIO(file[0]))
-    img1 = tf.keras.preprocessing.image.img_to_array(image1)
+#     image1 = Image.open(io.BytesIO(file[0]))
+#     img1 = tf.keras.preprocessing.image.img_to_array(image1)
 
-    image2 = Image.open(io.BytesIO(file[1]))
-    img2 = tf.keras.preprocessing.image.img_to_array(image2)
+#     image2 = Image.open(io.BytesIO(file[1]))
+#     img2 = tf.keras.preprocessing.image.img_to_array(image2)
 
-    with graph.as_default():
+#     with graph.as_default():
 
-        result = DeepFace.verify(img1, img2, model_name="Facenet")
-    return {"result": result}
+#         result = DeepFace.verify(img1, img2, model_name="Facenet")
+#     return {"result": result}
